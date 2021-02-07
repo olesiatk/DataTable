d52d87f2e1adcda0ede9db7f5420ff0bbb188bfb
Olesia <olesia.tk@gmail.com>
2021-02-07 15:14:48 +0200

    Add half of validation

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     tableHead.unshift('ID');
     tableHead.push('Dublicate with');
 
+    //
     const headLowerCase = tableHead.map(header => header.toLowerCase());
     if (headLowerCase.includes('email') 
         && headLowerCase.includes('phone')
@@ -24,36 +25,133 @@ function App() {
       console.log ('bad');
     }
 
+
+
     const tableBody = tableDataString.slice(1)
-      .map((string, i) => (`${i+1}; `+string+'; ').split(';')
+      .map((user, i) => (`${i+1}; `+user+'; ').split(';')
         .map((userData, index) => {
           let userObj = {
             name: tableHead[index],
             value: userData.trim(),
-            validation: false,
+            validation: true,
           }
 
           //check validation
-          switch (userObj.name) {
-            case ('Age'):
-              if (userObj.value >= 21) {
-                userObj.validation = true;
-              }
-              break;
-            default:
-              break;
-          }
+
           return userObj;
         })
       )
         .slice(0,-1);
 
-    const columns = tableHead.map(header => ({
+    // functions for validation (start)
+    function checkNumber(number) {
+      let x10 = number.slice(-10);
+      if (!Number.isInteger(+x10)) {
+        return false;
+      }
+      if (number.length >12 || number.length<10) {
+        return false;
+      } 
+      if (number.length === 12) {
+        x10 = number.slice(2);
+        if (number.slice(0,2) !== '+1') {
+          return false;
+        }
+      }
+      if (number.length === 11) {
+        x10 = number.slice(2);
+        if (number.slice(0,1) !== '1') {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    function checkAge(age) {
+      if (age < 21 || !Number.isInteger(+age)) {
+        return false;
+      }
+      return true;
+    }
+    // end functions for validation
+
+
+    // set date with check validation
+    const usersData = tableBody.map((user, i) => user.map((userData, dataIndex) => {
+      switch (userData.name.toLowerCase()) {
+        case ('phone'):
+          userData.validation = checkNumber(userData.value);
+          if (userData.validation && userData.value.length===10){
+            userData.value = '+1' + userData.value;
+          }
+          if (userData.validation && userData.value.length===11){
+            userData.value = '+' + userData.value;
+          }
+          break;
+        case ('age'):
+            userData.validation = checkAge(userData.value);
+          break;
+        case ('experience'):
+            console.log(user[dataIndex-1], userData);
+            if (!Number.isInteger(+userData.value) 
+                || userData.value < 0 
+                || userData.value > user[dataIndex-1].value
+              ) {
+              userData.validation = false;
+            }
+          break;
+        case ('yearly income'):
+          if (isNaN(+userData.value) || +userData.value > 1000000) {
+            userData.validation = false;
+          } else if (userData.value) {
+            userData.value = (Math.round(userData.value * 100)/100).toFixed(2);
+          }
+          break;
+        case ('has children'):
+            userData.validation = (userData.value.toLowerCase() ==='true'
+              || userData.value.toLowerCase() ==='false'
+              || userData.value ===''
+            ) 
+            ? true
+            : false;
+            if (userData.value === '') {
+              userData.value = 'FALSE';
+            }
+            break;
+        default:
+          break;
+      }
+      return userData;
+    }));
+
+    // set data with check unique Phone and Email
+    const usersDataCheckUnique = usersData.map((user, i) => user.map((userData, iDate) => {
+      if (userData.name.toLowerCase() === 'phone') {
+        const dublicateData = tableBody.find((user, n) => n !== i && user.find(userDataCheck => userDataCheck.value === userData.value));
+        if (dublicateData) {
+          user[user.length-1].value = dublicateData[0].value;
+          userData.validation = false;
+        }
+      }
+      if (userData.name.toLowerCase() === 'email') {
+        const dublicateData = tableBody.find((user, n) => n !== i && user.find(userDataCheck => userDataCheck.value.toLowerCase() === userData.value.toLowerCase()));
+        if (dublicateData) {
+          user[user.length-1].value = dublicateData[0].value;
+          userData.validation = false;
+        }
+      }
+      return userData;
+    }));
+
+
+    // set headers
+    const headers = tableHead.map(header => ({
       name: header,
     }));
- 
-    setData(tableBody);
-    setColumns(columns);
+
+    // set state
+    setData(usersDataCheckUnique);
+    setColumns(headers);
   }
  
   // handle file upload
@@ -71,11 +169,13 @@ function App() {
     }
   }
 
+  // function for change name of button
   const hiddenFileInput = React.useRef(null);
   const handleClick = () => {
     hiddenFileInput.current.click();
   };
 
+  // APP RETURN
   return (
     <div>
       <header className="App-header">
